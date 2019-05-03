6debecb77ee16b898cd94f1f19b0cb6f6cb23a65
Josh Reichardt <josh.reichardt@gmail.com>
2019-05-03 13:15:20 -0700

    Figure out slow builds

--- .drone/build_and_install.sh
+++ .drone/build_and_install.sh
@@ -11,23 +11,6 @@ set -xeuo pipefail
 
 # Conda setup
 
-# Create conda user with the same uid as the host, so the container can write
-# to mounted volumes
-# Adapted from https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
-#USER_ID=${HOST_USER_ID:-9001}
-#useradd --shell /bin/bash -u "$USER_ID" -G lucky -o -c "" -m conda
-#export HOME=/home/conda
-#export USER=conda
-#export LOGNAME=conda
-#export MAIL=/var/spool/mail/conda
-#export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/conda/bin
-#export supkg="su-exec"
-#
-#chown conda:conda $HOME
-#cp -R /etc/skel $HOME && chown -R conda:conda $HOME/skel && (ls -A1 $HOME/skel | xargs -I {} mv -n $HOME/skel/{} $HOME) && rm -Rf $HOME/skel
-#cp /root/.condarc $HOME/.condarc && chown conda:conda $HOME/.condarc
-#cd $HOME
-
 # Source the base Conda environment
 . /root/archiconda3/bin/activate || true
 . /opt/conda/bin/activate || true
