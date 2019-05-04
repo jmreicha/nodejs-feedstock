4ff824eb2ffe2c9a5ae36b232d1f9d4f16278881
Josh Reichardt <josh.reichardt@gmail.com>
2019-05-04 08:48:54 -0700

    More fun debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+set -x
+
 # scrub -std=... flag which conflicts with builds
 export CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-std=[^ ]*@@g')
+
+echo "Test CPU count: $(grep -c ^processor /proc/cpuinfo)"
+echo "Conda CPU count: ${CPU_COUNT}"
+exit
 
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
