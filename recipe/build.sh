fa4e0f0c93a4b5670ad7ff454f1168717bddbcf6
Josh Reichardt <josh.reichardt@gmail.com>
2019-05-05 07:58:09 -0700

    Testing cpu settings

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,13 @@ set -x
 # scrub -std=... flag which conflicts with builds
 export CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-std=[^ ]*@@g')
 
-echo "Test CPU count: $(grep -c ^processor /proc/cpuinfo)"
+echo "Test CPU count: $(nproc)"
 echo "Conda CPU count: ${CPU_COUNT}"
-exit
 
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
 ./configure --prefix=$PREFIX --without-snapshot
-make -j${CPU_COUNT}
+make -j$(nproc)
 make install
 
 node -v
