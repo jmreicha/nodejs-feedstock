c4e6118789ab3bf5fd988dc9472e5a659c86069a
Josh Reichardt <josh.reichardt@gmail.com>
2019-05-06 10:54:06 -0700

    Clean up the build

--- .drone/build_steps.sh
+++ .drone/build_steps.sh
@@ -5,53 +5,17 @@
 # changes to this script, consider a proposal to conda-smithy so that other feedstocks can also
 # benefit from the improvement.
 
-set -euo pipefail
-
-# TODO Figure out slowness and don't export variables
-
-# Conda setup
-
-# Create conda user with the same uid as the host, so the container can write
-# to mounted volumes
-# Adapted from https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
-USER_ID=${HOST_USER_ID:-9001}
-useradd --shell /bin/bash -u "$USER_ID" -G lucky -o -c "" -m conda
-export HOME=/home/conda
-export USER=conda
-export LOGNAME=conda
-export MAIL=/var/spool/mail/conda
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/conda/bin
-export supkg="su-exec"
-
-chown conda:conda $HOME
-cp -R /etc/skel $HOME && chown -R conda:conda $HOME/skel && (ls -A1 $HOME/skel | xargs -I {} mv -n $HOME/skel/{} $HOME) && rm -Rf $HOME/skel
-cp /root/.condarc $HOME/.condarc && chown conda:conda $HOME/.condarc
-cd $HOME
-
-# Source the base Conda environment
-. /opt/conda/bin/activate
-
-# Build
-
+set -xeuo pipefail
 export PYTHONUNBUFFERED=1
-#export FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
-export FEEDSTOCK_ROOT="${CI_WORKSPACE}"
-export RECIPE_ROOT="${FEEDSTOCK_ROOT}/recipe"
+export FEEDSTOCK_ROOT="${FEEDSTOCK_ROOT:-/home/conda/feedstock_root}"
+export RECIPE_ROOT="${RECIPE_ROOT:-/home/conda/recipe_root}"
 export CI_SUPPORT="${FEEDSTOCK_ROOT}/.ci_support"
-export ARTIFACTS="${FEEDSTOCK_ROOT}/build_artifacts"
-#export FEEDSTOCK_ROOT=/home/conda/feedstock_root
-#export RECIPE_ROOT=/home/conda/recipe_root
-#export CI_SUPPORT=/home/conda/feedstock_root/.ci_support
 export CONFIG_FILE="${CI_SUPPORT}/${CONFIG}.yaml"
-
-mkdir -p "${ARTIFACTS}"
-
-#root-dir: /home/conda/feedstock_root/build_artifacts
 
 cat >~/.condarc <<CONDARC
 
 conda-build:
- root-dir: ${ARTIFACTS}
+ root-dir: ${FEEDSTOCK_ROOT}/build_artifacts
 
 CONDARC
 
@@ -71,6 +35,4 @@ if [[ "${UPLOAD_PACKAGES}" != "False" ]]; then
     upload_package "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 fi
 
-touch "${ARTIFACTS}/conda-forge-build-done-${CONFIG}"
-# verify that the end of the script was reached
-#test -f "${ARTIFACTS}/conda-forge-build-done-${CONFIG}"
+touch "${FEEDSTOCK_ROOT}/build_artifacts/conda-forge-build-done-${CONFIG}"
